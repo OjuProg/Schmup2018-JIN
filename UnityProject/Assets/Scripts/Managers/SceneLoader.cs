948cb8b2f6e61f2f9378d211ea555b67d767249d
Oju <oju.programming@gmail.com>
2018-09-23 13:45:05 +0200

    Linked Main Menu and Game Mode

---
+++ UnityProject/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
