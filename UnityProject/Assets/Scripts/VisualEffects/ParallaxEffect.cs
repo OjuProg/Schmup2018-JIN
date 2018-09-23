eb103bce012c001687aa9f96a44401b012d0ff7b
Oju <oju.programming@gmail.com>
2018-09-23 16:54:23 +0200

    Added parallax effects for background & Moutains background

---
+++ UnityProject/Assets/Scripts/VisualEffects/ParallaxEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxEffect : MonoBehaviour {
+
+    [SerializeField]
+	private List<MovingBackgroundElement> backgroundElements;
+
+    void Update()
+    {
+        for(int index = 0; index < backgroundElements.Count; index++)
+        {
+            MovingBackgroundElement element = backgroundElements[index];
+            if (element != null && element.backgroundElement != null)
+            {
+                Renderer renderer = element.backgroundElement.GetComponent<Renderer>();
+                if(renderer != null)
+                {
+                    Vector2 offset = new Vector2(Time.time * element.speed, 0);
+                    renderer.material.mainTextureOffset = offset;
+                }
+            }
+        }
+    }
+
+}
